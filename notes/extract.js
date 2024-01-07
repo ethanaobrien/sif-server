eca987b525cf56af1425a879096e37de7719e198
Ethan O'Brien <ethan.a.obrien@gmail.com>
2024-01-07 00:58:29 -0600

    uhh, no

--- notes/extract.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const src = JSON.parse(fs.readFileSync('live_notes.json', 'utf8'))
-
-for (chart of src) {
-    console.log("Saving", chart.notes_setting_asset)
-    fs.writeFileSync(chart.notes_setting_asset, chart.json)
-}
